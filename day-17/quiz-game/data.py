25a26571d29aae055bd7af3a51ba5be5d291096b
Momchil Kolev <26323192+MomchilKolev@users.noreply.github.com>
2023-12-31 11:17:16 +0200

    feat: day 17 - quiz game

---
+++ day-17/quiz-game/data.py
@@ -0,0 +1,54 @@
+question_data = [
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "General Knowledge",
+        "question": "When you cry in space, your tears stick to your face.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean", "difficulty": "easy", "category": "General Knowledge",
+        "question": "Scotland voted to become an independent country during the referendum from September 2014.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean", "difficulty": "medium", "category": "General Knowledge",
+        "question": "Albert Einstein had trouble with mathematics when he was in school.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean", "difficulty": "medium", "category": "General Knowledge",
+        "question": "The French word to travel is &quot;Travail&quot;", "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean", "difficulty": "medium", "category": "General Knowledge",
+        "question": "Popcorn was invented in 1871 by Frederick W. Rueckheim in the USA where he sold the snack on the streets of Chicago.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean", "difficulty": "easy", "category": "General Knowledge",
+        "question": "Slovakia is a member of European Union-", "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean", "difficulty": "medium", "category": "General Knowledge",
+        "question": "The pickled gherkin was first added to hamburgers because a US health law required all fast-food to include a source of Vitamin C.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean", "difficulty": "medium", "category": "General Knowledge",
+        "question": "&quot;Santa Claus&quot; is a variety of melon.", "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean", "difficulty": "easy", "category": "General Knowledge",
+        "question": "Nutella is produced by the German company Ferrero.", "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean", "difficulty": "medium", "category": "General Knowledge",
+        "question": "The scientific name for the Southern Lights is Aurora Australis?",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    }]
